6d90ca7be3337f76cd1547df23a04c517ef355bf
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-19 20:39:05 +0000

    Tests

---
+++ src/server/housepricehistory/tests/TestIngestScript.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+
+import unittest
+from scripts.ingest_data import getDataEntryInsertSQL, checkPrintProgress, insertEntries, main as ingest_data_main
+
+class TestIngestScript(unittest.TestCase):
+	def testInsertEntries(self):
+		self.fail()
+
+	def testGetDataEntryInsertSQL(self):
+		self.fail()
+
+	def testCheckPrintProgress(self):
+		self.fail()
+
+	def testMain(self):
+		self.fail()
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
---
+++ src/server/housepricehistory/tests/TestViews.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+
+import unittest
+from src.server.housepricehistory.housepricehistory.views import *
+
+class TestIngestScript(unittest.TestCase):
+	def testIndex(self):
+		self.fail()
+
+	def testHistoryData(self):
+		self.fail()
+
+	def testHistoryDataWithTime(self):
+		self.fail()
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
