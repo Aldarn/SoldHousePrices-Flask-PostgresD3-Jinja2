552514e2e31cf737ba3a34dd8abc3b7b053ecbd2
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-21 20:21:09 +0000

    SoldDataService now correctly calculating and outputting values

--- src/server/housepricehistory/housepricehistory/services/SoldDataService.py
+++ src/server/housepricehistory/housepricehistory/services/SoldDataService.py
@@ -1,33 +1,37 @@
 import time
 import logging
 import getpass
+from collections import defaultdict
 from ..models import SoldProperty
 
 logger = logging.getLogger(__name__)
 
 def getAllAveragePriceData():
+	# Get all properties ordered by descending date
 	soldProperty = SoldProperty.objects.all().order_by('-date')[:1]
-	dateAverages = {}
+
+	# Create a date averages dictionary that defaults to holding prices and counts for different property types on specific days
+	dateAverages = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
+	# Populate the date averages
 	for property in soldProperty:
 		propertyDay = time.strftime("%d/%m/%Y")
-		if propertyDay in dateAverages:
-			if property.type in dateAverages[propertyDay]:
-				dateAverages[propertyDay][property.type]["total"] += property.price
-				dateAverages[propertyDay][property.type]["count"] += 1
-			else:
-				dateAverages[propertyDay][property.type]["total"] = property.price
-				dateAverages[propertyDay][property.type]["count"] = 1
+		dateAverages[propertyDay][property.type]["total"] += property.price
+		dateAverages[propertyDay][property.type]["count"] += 1
 
 	logger.debug("date averages: %s" % dateAverages)
 
 	data = "Date\tFlats\tTerraced\tDetached\tSemi-Detached\n"
 	for date, averages in dateAverages.iteritems():
-		data += "%s\t%s\t%s\t%s\t%s\n" % (date, getAverage(averages["F"]), getAverage(averages["T"]),
-			getAverage(averages["D"]), getAverage(averages["S"]))
+		data += "%s\t%s\t%s\t%s\t%s\n" % (date, _getAverage(averages["F"]), _getAverage(averages["T"]),
+			_getAverage(averages["D"]), _getAverage(averages["S"]))
 
 	return data
 
-def getAverage(averageDict):
+def _getAverage(averageDict):
+	# Should dates with no data be blank instead of 0?
+	if len(averageDict) == 0:
+		return 0
 	return int(float(averageDict["total"]) / float(averageDict["count"]))
 
 def getAveragePriceData(startTimestamp, endTimestamp):
