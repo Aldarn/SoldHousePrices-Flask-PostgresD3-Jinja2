dbeb8434d58a5590bd0b0606f9d6532cd3e592f0
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-21 15:08:54 +0000

    Fixed some bugs in the ingest script when parsing csv.

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -3,14 +3,16 @@
 import psycopg2
 import time
 import datetime
+import csv
+from itertools import chain
 
 DATA_TO_CSV_COLUMN = {
 	"uid": 0,
 	"price": 1,
 	"date": 2,
-	"postCode": 3,
+	"postcode": 3,
 	"type": 4, # D = Detached, S = Semi-Detached, T = Terraced, F = Flats/Maisonettes
-	"isOld": 5, # Y = a newly built property, N = an established residential building
+	"isold": 5, # Y = a newly built property, N = an established residential building
 	"duration": 6, # Relates to the tenure: F = Freehold, L-Leasehold etc.
 	"paon": 7, # Primary Addressable Object Name. If there is a sub-building for example the building is divided into flats, see Secondary Addressable Object Name (SAON).
 	"saon": 8, # Secondary Addressable Object Name. If there is a sub-building, for example the building is divided into flats, there will be a SAON.
@@ -26,23 +28,35 @@ def insertEntries(db, cursor, entries):
 	Inserts a batch of entries.
 	:param entries: List of entry insert SQLs.
 	"""
-	# Run the SQL
-	cursor.execute("INSERT INTO housepricehistory_soldproperty ('id', '%s') VALUES %s;", ("', '".join(DATA_TO_CSV_COLUMN.keys()), ",".join(entries)))
+	# Get the columns to use in the correct order
+	columns = ", ".join(sorted(DATA_TO_CSV_COLUMN, key = lambda k: DATA_TO_CSV_COLUMN[k]))
+
+	# Create the prepared statement
+	insertSql = "INSERT INTO housepricehistory_soldproperty (%s) VALUES %s;" % (columns,
+		("(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s), " * len(entries))[:-2])
+
+	# Execute with all entry data unpacked
+	cursor.execute(insertSql, tuple(chain(*entries)))
 	db.commit()
 
-def getDataEntrySQL(entry):
+def getFormattedEntry(entry):
 	"""
-	Gets the insert SQL for an entry.
-	:param entry: The entry to get the SQL for.
-	:return: The insert SQL for the entry.
+	Converts the entry into a list of values and formats each where necessary.
+	:param entry: The entry in the data file to format.
+	:return: Formatted list of entry values.
 	"""
-	entryData = entry.replace('"', '').split(",") # Remove all quotes and split into values
-	entryData[0].replace('{', '').replace('}', '') # Remove braces from the uid
+	# Remove braces from the uid
+	entry[DATA_TO_CSV_COLUMN["uid"]] = entry[DATA_TO_CSV_COLUMN["uid"]].replace('{', '').replace('}', '')
 
 	# Convert the date into a timestamp
-	entryData[DATA_TO_CSV_COLUMN["date"]] = time.mktime(datetime.datetime.strptime(entryData[DATA_TO_CSV_COLUMN["date"]], "%Y-%m-%d %H:%M").timetuple())
+	entry[DATA_TO_CSV_COLUMN["date"]] = int(time.mktime(datetime.datetime.strptime(entry[DATA_TO_CSV_COLUMN["date"]],
+		"%Y-%m-%d %H:%M").timetuple()))
 
-	return "('%s')" % "', '".join([str(entryData[i]) for i in DATA_TO_CSV_COLUMN.values()])
+	# Remove record status if it exists
+	if len(entry) > len(DATA_TO_CSV_COLUMN):
+		del entry[-1]
+
+	return entry
 
 def checkPrintProgress(currentCount, total):
 	"""
@@ -68,15 +82,19 @@ def main(dataFile = "data/feb_2015_sold_data.csv"):
 	with open(dataFile, 'r') as fileHandle:
 		processedCount = 0
 		totalEntries = 83245
-		currentInserts = []
-		for entry in fileHandle:
+		currentEntries = []
+
+		# CSV module will split each line of data correctly and remove field quotes
+		for entry in csv.reader(fileHandle):
 			checkPrintProgress(processedCount, totalEntries)
-			currentInserts.append(getDataEntrySQL(entry))
+			processedCount += 1
+
+			currentEntries.append(getFormattedEntry(entry))
 
 			# TODO: Replace arbitrary insert count with a check for the maximum insert sql length
-			if len(currentInserts) == 100:
-				insertEntries(db, cursor, currentInserts)
-				del currentInserts[:]
+			if len(currentEntries) == 1:
+				insertEntries(db, cursor, currentEntries)
+				del currentEntries[:]
 
 	cursor.close()
 	db.close()
