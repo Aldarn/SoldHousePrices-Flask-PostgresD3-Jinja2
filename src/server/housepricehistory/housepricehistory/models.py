45eba16930052e3e33b52222dc527098f83aea66
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-21 15:08:34 +0000

    Fixed SQL to remove casing in postgres column names

--- src/server/housepricehistory/housepricehistory/models.py
+++ src/server/housepricehistory/housepricehistory/models.py
@@ -4,9 +4,9 @@ class SoldProperty(models.Model):
 	uid = models.CharField(max_length = 36, primary_key = True)
 	price = models.IntegerField()
 	date = models.IntegerField()
-	postCode = models.CharField(max_length = 8)
+	postcode = models.CharField(max_length = 8)
 	type = models.CharField(max_length = 1)
-	isOld = models.BooleanField()
+	isold = models.BooleanField()
 	duration = models.CharField(max_length = 1)
 	paon = models.CharField(max_length = 255) # Could it go above this?
 	saon = models.CharField(max_length = 255) # Could it go above this?
