e4ae2ae96cb27b5c01cfc3f4ef3b0653020d7070
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-22 18:59:15 +0000

    Created new view for soon to come improved graph

--- src/server/housepricehistory/housepricehistory/urls.py
+++ src/server/housepricehistory/housepricehistory/urls.py
@@ -4,12 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'housepricehistory.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls), name = "Admin"),
     url(r'^$', views.index, name = "Index"),
     url(r'^jinja$', views.jinja, name = "Jinja"),
     url(r'^averagePrices$', views.averagePrices, name = "Average Prices"),
+    url(r'^improved$', views.indexImproved, name = "Index Improved")
 )
--- src/server/housepricehistory/housepricehistory/views.py
+++ src/server/housepricehistory/housepricehistory/views.py
@@ -13,19 +13,22 @@ def index(request):
 	"""
 	return HttpResponse(render_to_string('history.jinja'))
 
+def indexImproved(request):
+	return HttpResponse(render_to_string('history-improved.jinja'))
+
 def jinja(request):
 	return HttpResponse(render_to_string('jinja.jinja'))
 
 def averagePrices(request):
 	"""
-	Gets sold price history JSON for use within the graphs.
+	Gets average price history TSV for use within the graphs.
 
 	Supports start and end timestamps to get snapshots of data.
 
-	:param request: HTTP Request
-	:return: history JSON
+	:param request: HTTP Request.
+	:return: Average sold prices TSV.
 	"""
-	if request.method == 'GET' and "start" in request.GET:
+	if request.method == 'GET' and "start" in request.GET and "end" in request.GET:
 		start = request.GET["start"]
 		end = request.GET["end"]
 		response = SoldDataService.getAveragePriceData(start, end)
