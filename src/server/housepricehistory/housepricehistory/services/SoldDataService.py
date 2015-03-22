3c036b8cfb88aeda425b2db8202b40b93e621d02
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-03-22 00:19:06 +0000

    Fixed ordering of data by date

--- src/server/housepricehistory/housepricehistory/services/SoldDataService.py
+++ src/server/housepricehistory/housepricehistory/services/SoldDataService.py
@@ -1,35 +1,48 @@
 import time
 import logging
-import getpass
+import datetime
 from collections import defaultdict
 from ..models import SoldProperty
 
 logger = logging.getLogger(__name__)
 
 def getAllAveragePriceData():
+	"""
+	Grabs all the properties and calculates the average price of each type on each day.
+
+	TODO: Really these values should be cached, otherwise this would simply be too intensive for the entire data set.
+	:return: TSV data.
+	"""
+
 	# Get all properties ordered by descending date
-	soldProperty = SoldProperty.objects.all().order_by('-date')[:1]
+	soldProperty = SoldProperty.objects.all().order_by('-date')[:10000]
 
 	# Create a date averages dictionary that defaults to holding prices and counts for different property types on specific days
 	dateAverages = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
 	# Populate the date averages
 	for property in soldProperty:
-		propertyDay = time.strftime("%d/%m/%Y")
+		propertyDay = int((datetime.datetime.fromtimestamp(property.date).date() - datetime.date(1970, 1, 1)).total_seconds())
 		dateAverages[propertyDay][property.type]["total"] += property.price
 		dateAverages[propertyDay][property.type]["count"] += 1
 
 	logger.debug("date averages: %s" % dateAverages)
 
-	data = "Date\tFlats\tTerraced\tDetached\tSemi-Detached\n"
-	for date, averages in dateAverages.iteritems():
-		data += "%s\t%s\t%s\t%s\t%s\n" % (date, _getAverage(averages["F"]), _getAverage(averages["T"]),
+	data = "date\tFlats\tTerraced\tDetached\tSemi-Detached\n"
+	for date in sorted(dateAverages):
+		averages = dateAverages[date]
+		data += "%s\t%s\t%s\t%s\t%s\n" % (datetime.datetime.fromtimestamp(date).strftime("%d/%m/%Y"), _getAverage(averages["F"]), _getAverage(averages["T"]),
 			_getAverage(averages["D"]), _getAverage(averages["S"]))
 
 	return data
 
 def _getAverage(averageDict):
-	# Should dates with no data be blank instead of 0?
+	"""
+	Gets the average (mean) for the given dictionary containing the total and count.
+
+	:param averageDict: Average dictionary.
+	:return: Mean value or 0 if no values in dict.
+	"""
 	if len(averageDict) == 0:
 		return 0
 	return int(float(averageDict["total"]) / float(averageDict["count"]))
